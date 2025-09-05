93b28aaf6f371fb42c893202960544f23eb84108
hariniramamoorthi <hariniramamoorthi@digitalxc.com>
2025-09-05 22:11:00 +0530

    Removed scan packages in main application

--- src/main/java/com/mini/school/erp/course_service/CourseServiceApplication.java
+++ src/main/java/com/mini/school/erp/course_service/CourseServiceApplication.java
@@ -3,10 +3,7 @@ package com.mini.school.erp.course_service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = {
-        "com.mini.school.erp.course_service",
-        "com.mini.erp.school.common_security"
-})
+@SpringBootApplication
 public class CourseServiceApplication {
 
 	public static void main(String[] args) {
